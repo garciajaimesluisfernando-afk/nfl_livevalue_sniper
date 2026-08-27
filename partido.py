db1238b10d5ea7f4804dc9c20081f113d7833306
Fernando Garcia <garciajaimesluisfernando@gmail.com>
2026-08-27 00:40:12 -0600

    feat: implement base logic and object classes for live value sniper

---
+++ partido.py
@@ -0,0 +1,44 @@
+class PartidoEnVivo:
+    def __init__(self, nombre_local, nombre_visitante, momio_pregame_local, momio_pregame_visitante):
+        self.nombre_local =nombre_local
+        self.nombre_visitante = nombre_visitante
+        self.momio_pregame_local =momio_pregame_local
+        self.momio_pregame_visitante = momio_pregame_visitante
+        self.marcador_local = 0
+        self.marcador_visitante = 0
+        self.tiempo_restante = "Q1 15:00"
+        self.momio_actual_local = self.momio_pregame_local
+        self.momio_actual_visitante = self.momio_pregame_visitante
+        
+        
+    def actualizar_datos(self, marcador_local_actualizado, marcador_visitante_actualizado, tiempo_restante_actualizado, 
+                         momio_actualizado_local, momio_actualizado_visitante):
+        
+        self.marcador_local = marcador_local_actualizado
+        self.marcador_visitante = marcador_visitante_actualizado
+        self.tiempo_restante = tiempo_restante_actualizado
+        self.momio_actual_local = momio_actualizado_local
+        self.momio_actual_visitante = momio_actualizado_visitante
+        
+        
+    def verificar_alerta_valor(self):
+        
+        
+        if self.momio_pregame_local < self.momio_pregame_visitante:
+            
+            diferencia = self.marcador_visitante - self.marcador_local
+            
+            
+            if diferencia >= 7 and self.momio_actual_local > 105:
+                return True
+            
+            
+            
+        elif self.momio_pregame_local > self.momio_pregame_visitante:
+            
+            diferencia = self.marcador_local - self.marcador_visitante
+            
+            if diferencia >= 7 and self.momio_actual_visitante > 105:
+                return True
+            
+        return False
